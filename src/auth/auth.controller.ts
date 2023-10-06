6f250b9aba7b50e46114c2cdedcb248e1b3ef1a0
bhandekunal16 <bhandekunal16@gmail.com>
2023-10-06 13:54:54 +0530

    update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
