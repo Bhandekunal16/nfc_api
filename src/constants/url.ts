925c8bacf64199f5c12c297b81b082cde664d664
bhandekunal16 <bhandekunal16@gmail.com>
2023-10-06 10:45:05 +0530

    update

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,5 +1,5 @@
 export const url = (name) => {
   return {
-    location: `https://mybusiness.googleapis.com/v4/name=${name}/locations:fetchVerificationOptions`,
+    location: `https://mybusiness.googleapis.com/v4/name=${name}/locations`,
   };
 };
