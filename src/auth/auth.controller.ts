9ec49d434289bc2c6695690676f15956ef386e79
bhandekunal16 <bhandekunal16@gmail.com>
2023-10-09 10:23:59 +0530

    update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,22 +2,22 @@ import { Controller, Get, Post, Body, Query } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { getAccount } from './dto/get-account.dto';
 import { createAccount } from './dto/create-account.dto';
+import { authorization } from 'src/routes/routes';
 
-@Controller('auth')
+@Controller(authorization.Controller)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get('/account')
+  @Get(authorization.GetAccount)
   async getAccount(@Query('name') name: getAccount) {
     try {
-      console.log(name);
       return await this.authService.getAccount(name);
     } catch (error) {
       return { res: error, status: false, msg: 'error' };
     }
   }
 
-  @Post('create/businessAccount')
+  @Post(authorization.CreateBusinessAccount)
   async createAccount(@Body() body: createAccount) {
     try {
       return await this.authService.createBusiness(body);
---
+++ src/routes/routes.ts
@@ -0,0 +1,5 @@
+export const authorization = {
+  Controller: 'auth',
+  GetAccount: 'get/account',
+  CreateBusinessAccount: 'create/businessAccount',
+};
