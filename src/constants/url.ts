e2132aef9a44468748d0c3bf245261e329131de2
bhandekunal16 <bhandekunal16@gmail.com>
2023-10-09 09:26:10 +0530

    update

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,8 +1,8 @@
 export const url = (name) => {
   return {
-    location: `https://mybusiness.googleapis.com/v4/name=${name}/locations`,
-    getAllReview: `https://mybusiness.googleapis.com/v4/name=${name}/locations/reviews/`,
-    getAllReviewOwner: `https://mybusiness.googleapis.com/v4/parent=${name}/locations/reviews/`,
+    location: `https://mybusiness.googleapis.com/v4/${name}/locations`,
+    getAllReview: `https://mybusiness.googleapis.com/v4/${name}/locations/reviews/`,
+    getAllReviewOwner: `https://mybusiness.googleapis.com/v4/${name}/locations/reviews/`,
     createBusiness: `mybusinessaccountmanagement.googleapis.com/v1/accounts`,
     getInfo: `https://mybusinessaccountmanagement.googleapis.com/v1/accounts/${name}`,
   };
